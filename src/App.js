bc4933c261f0a9c21c9fc4d4e8d13866710e1ce1
Severo-Yannick <severoyannick@gmail.com>
2023-07-14 11:06:05 +0200

    toast a message with duration

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import { setupIonicReact, IonButton } from "@ionic/react";
+import { setupIonicReact, IonButton, useIonToast } from "@ionic/react";
 import "@ionic/react/css/core.css";
 
 setupIonicReact();
 
 function App() {
+  const [presentToast] = useIonToast();
+
+  const handleClick = () => {
+    presentToast({
+      message: "Hello Ionic 3 seconds !",
+      duration: 3000,
+    });
+  };
+
   return (
     <div>
       <header>
         <h1>My App</h1>
       </header>
       <main>
-        <IonButton>Click me !</IonButton>
+        <IonButton onClick={handleClick}>Click me !</IonButton>
       </main>
     </div>
   );
